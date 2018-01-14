26123be9d1bbc05f43c9508d638335d42d0a58b5
blisstoner <blisstoner@gmail.com>
2018-01-15 08:41:02 +0900

    Create A.cpp

---
+++ Educational Round 36/A.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+int main(void) {
+	int n, k;
+	scanf("%d %d", &n, &k);
+	int mn = 9999;
+	for (int i = 0; i < n; i++) {
+		int a;
+		scanf("%d", &a);
+		if (k % a != 0)
+			continue;
+		mn = min(mn, k / a);
+	}
+	printf("%d", mn);
+}
