e4e635e52232b3967bf895e1a0533b39ed476fba
blisstoner <blisstoner@gmail.com>
2018-02-16 21:38:55 +0900

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <functional>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> PAIR;
+#define X first
+#define Y second
+int main(void){
+
+}
