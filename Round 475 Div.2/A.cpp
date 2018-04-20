2b5c2de05901a020ff1c15b4bd328f62114e4738
blisstoner <20028331+blisstoner@users.noreply.github.com>
2018-04-21 04:43:35 +0900

    A - Splits
    Math

---
+++ Round 475 Div.2/A.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <functional>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+#define X first
+#define Y second
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	printf("%d", (n + 2) / 2);
+}
