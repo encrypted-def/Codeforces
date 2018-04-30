4271211b5a2769557ff7ab6b9f22b7d3261dc855
blisstoner <20028331+blisstoner@users.noreply.github.com>
2018-04-30 15:55:38 +0900

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,14 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 #include <vector>
 #include <algorithm>
 #include <queue>
 #include <stack>
+#include <set>
+#include <map>
 #include <functional>
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 int dx[4] = { 1,-1,0,0 };
 int dy[4] = { 0,0,1,-1 };
+int ddx[8] = { 0,0,1,1,1,-1,-1,-1 }, ddy[8] = { 1,-1,1,0,-1,1,0,-1 };
+ll MOD = 1000000000;
+ll POW(ll a, ll b, ll MMM = MOD) { ll ret = 1; for (; b; b >>= 1, a = (a*a) % MMM)if (b & 1)ret = (ret*a) % MMM; return ret; }
+ll gcd(ll a, ll b) { return b ? gcd(b, a%b) : a; }
+ll lcm(ll a, ll b) { if (a == 0 || b == 0)return a + b; return a*(b / gcd(a, b)); }
 #define X first
 #define Y second
 int main(void){
