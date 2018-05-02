c780b6fdd882573d20169c5b5c8f46142344f986
blisstoner <20028331+blisstoner@users.noreply.github.com>
2018-05-02 17:59:09 +0900

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -7,9 +7,16 @@
 #include <set>
 #include <map>
 #include <functional>
+#include <iostream>
+#include <tuple>
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef tuple<int, int, int> ti3;
+typedef tuple<int, int, int, int> ti4;
 int dx[4] = { 1,-1,0,0 };
 int dy[4] = { 0,0,1,-1 };
 int ddx[8] = { 0,0,1,1,1,-1,-1,-1 }, ddy[8] = { 1,-1,1,0,-1,1,0,-1 };
@@ -19,6 +26,27 @@ ll gcd(ll a, ll b) { return b ? gcd(b, a%b) : a; }
 ll lcm(ll a, ll b) { if (a == 0 || b == 0)return a + b; return a*(b / gcd(a, b)); }
 #define X first
 #define Y second
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define pb(x) push_back(x)
+#define all(x) (x).begin(), (x).end()
+#define sf1(a) cin >> a
+#define sf2(a,b) cin >> a >> b
+#define sf3(a,b,c) cin >> a >> b >> c
+#define sf4(a,b,c,d) cin >> a >> b >> c >> d
+#define sf5(a,b,c,d,e) cin >> a >> b >> c >> d >> e
+#define sf6(a,b,c,d,e,f) cin >> a >> b >> c >> d >> e >> f
+#define pf1(a) cout << a << ' '
+#define pf2(a,b) cout << a << ' ' << b
+#define pf3(a,b,c) cout << a << ' ' << b << ' '<< c << ' '
+#define pf4(a,b,c) cout << a << ' ' << b << ' '<< c << ' '<< d << ' '
+#define pf5(a,b,c) cout << a << ' ' << b << ' '<< c << ' '<< d << ' '<< e << ' '
+#define pf6(a,b,c) cout << a << ' ' << b << ' '<< c << ' '<< d << ' '<< e << ' ' << f << ' '
+#define pf1l(a) cout << a << '\n'
+#define pf2l(a,b) cout << a << '\n' << b
+#define pf3l(a,b,c) cout << a << '\n' << b << '\n' << c << '\n'
+#define pf4l(a,b,c) cout << a << '\n' << b << '\n' << c << '\n' << d << '\n'
+#define pf5l(a,b,c) cout << a << '\n' << b << '\n' << c << '\n' << d << '\n' << e << '\n'
+#define pf6l(a,b,c) cout << a << '\n' << b << '\n' << c << '\n' << d << '\n' << e << '\n' << f << '\n'
 int main(void){
 
 }
