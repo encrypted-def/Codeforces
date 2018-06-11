17d46e8f3d21dde89ba0edb87456455adfddc4f7
blisstoner <20028331+blisstoner@users.noreply.github.com>
2018-06-11 21:42:06 +0900

    E - Post Lamps
    Brute force, Greedy

---
+++ Educational Round 45/E.cpp
@@ -0,0 +1,137 @@
+# pragma GCC optimize ("O3")
+# pragma GCC optimize ("Ofast")
+# pragma GCC optimize ("unroll-loops")
+
+#include <bits/stdc++.h>
+
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef tuple<int, int, int> ti3;
+typedef tuple<int, int, int, int> ti4;
+typedef stack<int> si;
+typedef queue<int> qi;
+typedef priority_queue<int> pqi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+typedef tuple<ll, ll, ll> tl3;
+typedef tuple<ll, ll, ll, ll> tl4;
+typedef stack<ll> sl;
+typedef queue<ll> ql;
+typedef priority_queue<ll> pql;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+int ddx[8] = { 0,0,1,1,1,-1,-1,-1 }, ddy[8] = { 1,-1,1,0,-1,1,0,-1 };
+ll POW(ll a, ll b, ll MMM) { ll ret = 1; for (; b; b >>= 1, a = (a*a) % MMM)if (b & 1)ret = (ret*a) % MMM; return ret; }
+ll GCD(ll a, ll b) { return b ? GCD(b, a%b) : a; }
+ll LCM(ll a, ll b) { if (a == 0 || b == 0)return a + b; return a * (b / GCD(a, b)); }
+ll INV(ll a, ll m) {
+	ll m0 = m, y = 0, x = 1;
+	if (m == 1)	return 0;
+	while (a > 1) {
+		ll q = a / m;
+		ll t = m;
+		m = a % m, a = t;
+		t = y;
+		y = x - q * y;
+		x = t;
+	}
+	if (x < 0) x += m0;
+	return x;
+}
+pll EXGCD(ll a, ll b) {
+	if (b == 0) return { 1,0 };
+	auto t = EXGCD(b, a%b);
+	return { t.second,t.first - t.second*(a / b) };
+}
+bool OOB(ll x, ll y, ll N, ll M) { return 0 > x || x >= N || 0 > y || y >= M; }
+#define X first
+#define Y second
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define pb(x) push_back(x)
+#define all(x) (x).begin(), (x).end()
+#define sf1(a) cin >> a
+#define sf2(a,b) cin >> a >> b
+#define sf3(a,b,c) cin >> a >> b >> c
+#define sf4(a,b,c,d) cin >> a >> b >> c >> d
+#define sf5(a,b,c,d,e) cin >> a >> b >> c >> d >> e
+#define sf6(a,b,c,d,e,f) cin >> a >> b >> c >> d >> e >> f
+#define pf1(a) cout << (a) << ' '
+#define pf2(a,b) cout << (a) << ' ' << (b) << ' '
+#define pf3(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << ' '
+#define pf4(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '
+#define pf5(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' '
+#define pf6(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << ' '
+#define pf0l() cout << '\n';
+#define pf1l(a) cout << (a) << '\n'
+#define pf2l(a,b) cout << (a) << ' ' << (b) << '\n'
+#define pf3l(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << '\n'
+#define pf4l(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << '\n'
+#define pf5l(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << '\n'
+#define pf6l(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << '\n'
+#define pfvec(V) for(auto const &t : V) pf1(t)
+#define pfvecl(V) for(auto const &t : V) pf1(t); pf0l()
+
+int N, M, K;
+int S[1000005];
+int A[1000005];
+vector<bool> street(1000005); // 1 : blocked
+int leftmost[1000005];
+int cont = 0;
+int calc(int dist) {
+	if (dist <= cont) return -1;
+	int pos = 0;
+	int cnt = 0;
+	while (pos < N) {
+		cnt++;
+		pos += dist;
+		if (pos >= N) break;
+		if (leftmost[pos] != pos) pos = leftmost[pos];
+	}
+	return cnt;
+}
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	sf3(N, M, K);
+	rep(i, 0, M) {
+		sf1(S[i]);
+		street[S[i]] = 1;
+	}
+	rep(i, 1, K + 1) sf1(A[i]);
+	if (street[0] == 1) {
+		pf1(-1);
+		return 0;
+	}
+	leftmost[0] = 0;
+	rep(i, 1, N + 1) {
+		if (street[i] == 1)
+			leftmost[i] = leftmost[i - 1];
+		else
+			leftmost[i] = i;
+	}
+	if (M != 0) {
+		int tmpcont = 1;
+		cont = 1;
+		rep(i, 1, M) {
+			if (S[i] == S[i - 1] + 1) {
+				tmpcont++;
+				if (tmpcont > cont) cont = tmpcont;
+			}
+			else
+				tmpcont = 1;
+		}
+	}
+	ll ans = 0x7f7f7f7f78f7f;
+	rep(i, 1, K + 1) {
+		int num = calc(i);
+		if (num == -1) continue;
+		ll val = 1ll * A[i] * num;
+		if (ans > val) ans = val;
+	}
+	if (ans == 0x7f7f7f7f78f7f)	pf1(-1);
+	else pf1(ans);
+}
