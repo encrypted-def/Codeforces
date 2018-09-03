2725d24c40297c711958dfce44aef95ec213fc80
blisstoner <20028331+blisstoner@users.noreply.github.com>
2018-09-03 16:37:23 +0900

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -5,7 +5,7 @@
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
-#include <ext/rope.h>
+#include <ext/rope>
 
 using namespace std;
 using namespace __gnu_pbds;
