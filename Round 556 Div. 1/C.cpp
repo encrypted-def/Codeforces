cd945540d2179c51ffcc5ab3566f612d5b2a2385
blisstoner <blisstoner@gmail.com>
2019-06-20 16:34:43 +0900

    C - And Reachability
    Dynamic Programming, Dijkstra

---
+++ Round 556 Div. 1/C.cpp
@@ -0,0 +1,160 @@
+# pragma GCC optimize ("O3")
+# pragma GCC optimize ("Ofast")
+# pragma GCC optimize ("unroll-loops")
+# pragma GCC target("sse,sse2,sse3,ssse3,sse4,avx,avx2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
+typedef unsigned int ui;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef tuple<int, int, int> ti3;
+typedef tuple<int, int, int, int> ti4;
+typedef stack<int> si;
+typedef queue<int> qi;
+typedef priority_queue<int> pqi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+typedef tuple<ll, ll, ll> tl3;
+typedef tuple<ll, ll, ll, ll> tl4;
+typedef stack<ll> sl;
+typedef queue<ll> ql;
+typedef priority_queue<ll> pql;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>ordered_set;
+
+const int dx[4] = { 1,0,-1,0 };
+const int dy[4] = { 0,1,0,-1 };
+const int ddx[8] = { 0,0,1,1,1,-1,-1,-1 }, ddy[8] = { 1,-1,1,0,-1,1,0,-1 };
+ll POW(ll a, ll b, ll MMM) { ll ret = 1; for (; b; b >>= 1, a = (a*a) % MMM)if (b & 1)ret = (ret*a) % MMM; return ret; }
+ll GCD(ll a, ll b) { return b ? GCD(b, a%b) : a; }
+ll LCM(ll a, ll b) { if (a == 0 || b == 0)return a + b; return a / GCD(a, b) * b; }
+ll INV(ll a, ll m) {
+  ll m0 = m, y = 0, x = 1;
+  if (m == 1)	return 0;
+  while (a > 1) {
+    ll q = a / m;
+    ll t = m;
+    m = a % m, a = t;
+    t = y;
+    y = x - q * y;
+    x = t;
+  }
+  if (x < 0) x += m0;
+  return x;
+}
+pll EXGCD(ll a, ll b) {
+  if (b == 0) return { 1,0 };
+  auto t = EXGCD(b, a%b);
+  return { t.second,t.first - t.second*(a / b) };
+}
+bool OOB(ll x, ll y, ll N, ll M) { return 0 > x || x >= N || 0 > y || y >= M; }
+#define X first
+#define Y second
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(a) ((int)(a.size()))
+#define sf1(a) cin >> a
+#define sf2(a,b) cin >> a >> b
+#define sf3(a,b,c) cin >> a >> b >> c
+#define sf4(a,b,c,d) cin >> a >> b >> c >> d
+#define sf5(a,b,c,d,e) cin >> a >> b >> c >> d >> e
+#define sf6(a,b,c,d,e,f) cin >> a >> b >> c >> d >> e >> f
+#define pf1(a) cout << (a) << ' '
+#define pf2(a,b) cout << (a) << ' ' << (b) << ' '
+#define pf3(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << ' '
+#define pf4(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '
+#define pf5(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' '
+#define pf6(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << ' '
+#define pf0l() cout << '\n';
+#define pf1l(a) cout << (a) << '\n'
+#define pf2l(a,b) cout << (a) << ' ' << (b) << '\n'
+#define pf3l(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << '\n'
+#define pf4l(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << '\n'
+#define pf5l(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << '\n'
+#define pf6l(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << '\n'
+#define pfvec(V) for(auto const &t : V) pf1(t)
+#define pfvecl(V) for(auto const &t : V) pf1(t); pf0l()
+
+
+
+int n,q;
+int a[300005];
+int nxt[300005][19];
+vi bitlist[300005];
+vi bucket[19];
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout << fixed;
+  cout.precision(20);
+///////////////////////////////////////////////
+  sf2(n,q);
+  rep(i,1,n+1){
+    sf1(a[i]);
+    int tmp = a[i];
+    rep(j,0,19){
+      if(tmp&1){
+        bucket[j].pb(i);
+        bitlist[i].pb(j);        
+      }
+      tmp >>= 1;
+    }
+  }  
+  for(int i = n; i >= 1; i--){
+    for(auto b : bitlist[i]){
+      if(bucket[b].back() == i) continue;
+      int nn = *(lower_bound(all(bucket[b]),i)+1);
+      nxt[i][b] = nn;
+      //pf3l(i,b,nn);
+      for(int j = 0; j < 19; j++){
+        if(nxt[nn][j] == 0) continue;
+        if(nxt[i][j] == 0) nxt[i][j] = nxt[nn][j];
+        else nxt[i][j] = min(nxt[i][j],nxt[nn][j]);
+      //  pf3l(i,j,nxt[i][j]);
+      }
+    }
+
+  }
+  int mn[20] = {0,};
+  while(q--){
+    int x,y;
+    sf2(x,y);
+    memset(mn,0x3f,sizeof(mn));
+    priority_queue<pii,vector<pii>,greater<pii> > PQ;
+    for(auto b : bitlist[x]){
+      mn[b] = x;
+      PQ.push({mn[b],b});
+    }
+    while(!PQ.empty()){
+      auto cur = PQ.top(); PQ.pop();
+      //pf2l(cur.X,cur.Y);
+      if(cur.X != mn[cur.Y]) continue;
+      rep(i,0,19){
+        if(nxt[cur.X][i] == 0 or nxt[cur.X][i] >= mn[i]) continue;
+        PQ.push({nxt[cur.X][i], i});
+        mn[i] = nxt[cur.X][i];
+      }
+    }
+    bool pos = 0;
+    for(auto b : bitlist[y]){
+      //if(mn[b] == 0x3f3f3f3f) continue;
+      if(mn[b] <= y){
+        pos = 1;
+        break;
+      }
+    }
+    //rep(i,0,19) pf1(mn[i]);
+    //pf0l();
+    if(pos) pf1l("Shi");
+    else pf1l("Fou");
+  }  
+}
